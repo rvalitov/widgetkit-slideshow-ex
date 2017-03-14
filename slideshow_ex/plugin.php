15c92f14c61d1cac0dfa72f3f83b3509ec0bd609
Ramil Valitov <ramilvalitov@gmail.com>
2017-03-14 17:55:46 +0300

    version update to v1.3.6

--- slideshow_ex/plugin.php
+++ slideshow_ex/plugin.php
@@ -16,9 +16,9 @@ return array(
 
     'main' => 'YOOtheme\\Widgetkit\\Widget\\Widget',
 	
-	'plugin_version' => 'v1.3.5',
+	'plugin_version' => 'v1.3.6',
 	
-	'plugin_date' => '08/02/2017',
+	'plugin_date' => '14/03/2017',
 	
 	'plugin_logo' => 'https://raw.githubusercontent.com/wiki/rvalitov/widgetkit-slideshow-ex/images/logo.jpg',
 	
